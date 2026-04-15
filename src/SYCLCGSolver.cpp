8789c8e8a904a25d377641367890c7b56f6b6028
matekelemen <kelemen.mate.12@gmail.com>
2026-04-15 15:58:49 +0200

    improve sycl cg

--- src/SYCLCGSolver.cpp
+++ src/SYCLCGSolver.cpp
@@ -8,6 +8,7 @@
 #include "packages/solvers/inc/SYCLCSROperator.hpp"
 #include "packages/solvers/inc/DiagonalOperator.hpp"
 #include "packages/solvers/inc/ConjugateGradients.hpp"
+#include "packages/utilities/inc/reorder.hpp"
 
 // --- Internal Includes ---
 #include "CiEApplication/SYCLCGSolver.hpp"
@@ -54,6 +55,10 @@ struct SYCLCGSolver<TS,TD>::Impl {
     std::shared_ptr<Operator> mpLhsOperator, mpPreconditioner;
 
     std::shared_ptr<Solver> mpSolver;
+
+    std::string mReordering;
+
+    std::vector<Index> mOrdering;
 }; // struct SYCLCGSolver::Impl
 
 
@@ -97,6 +102,8 @@ SYCLCGSolver<TS,TD>::SYCLCGSolver(Parameters Settings)
             mpImpl->mAbsoluteTolerance = Settings["absolute_tolerance"].GetDouble();
             mpImpl->mRelativeTolerance = Settings["relative_tolerance"].GetDouble();
             mpImpl->mVerbosity = Settings["verbosity"].GetInt();
+            mpImpl->mPreconditionerName = Settings["preconditioner"].GetString();
+            mpImpl->mReordering = Settings["reordering"].GetString();
         KRATOS_CATCH("")
 }
 
@@ -115,8 +122,38 @@ void SYCLCGSolver<TS,TD>::InitializeSolutionStep(
     typename TS::VectorType&,
     typename TS::VectorType&) {
         KRATOS_TRY
-            // Copy the matrix to the device.
             const std::size_t system_size = TS::Size1(rLhs);
+
+            // Compute and perform reordering if requested.
+            if (mpImpl->mReordering == "cuthill-mckee") {
+                mpImpl->mOrdering.resize(system_size);
+                cie::makeReordering<typename Impl::Index,typename Impl::Value>(
+                    mpImpl->mOrdering,
+                    {&rLhs.index1_data()[0], rLhs.index1_data().size()},
+                    {&rLhs.index2_data()[0], rLhs.index2_data().size()},
+                    {&rLhs.value_data()[0], rLhs.value_data().size()},
+                    cie::ReorderingStrategy::CuthillMcKee);
+            } else if (mpImpl->mReordering == "reverse-cuthill-mckee") {
+                mpImpl->mOrdering.resize(system_size);
+                cie::makeReordering<typename Impl::Index,typename Impl::Value>(
+                    mpImpl->mOrdering,
+                    {&rLhs.index1_data()[0], rLhs.index1_data().size()},
+                    {&rLhs.index2_data()[0], rLhs.index2_data().size()},
+                    {&rLhs.value_data()[0], rLhs.value_data().size()},
+                    cie::ReorderingStrategy::CuthillMcKee);
+            } else if (mpImpl->mReordering != "none") {
+                KRATOS_ERROR << "unknown reordering \"" << mpImpl->mReordering << "\n";
+            }
+
+            if (!mpImpl->mOrdering.empty()) {
+                cie::reorder<typename Impl::Index,typename Impl::Value>(
+                    mpImpl->mOrdering,
+                    {&rLhs.index1_data()[0], rLhs.index1_data().size()},
+                    {&rLhs.index2_data()[0], rLhs.index2_data().size()},
+                    {&rLhs.value_data()[0], rLhs.value_data().size()});
+            }
+
+            // Copy the matrix to the device.
             mpImpl->mRowExtents = mpImpl->mpIndexSpace->makeVector(rLhs.index1_data().size());
             mpImpl->mColumnIndices = mpImpl->mpIndexSpace->makeVector(rLhs.index2_data().size());
             mpImpl->mEntries = mpImpl->mpSpace->makeVector(rLhs.value_data().size());
@@ -151,20 +188,18 @@ void SYCLCGSolver<TS,TD>::InitializeSolutionStep(
             mpImpl->mRhs = mpImpl->mpSpace->makeVector(system_size);
 
             // Construct a preconditioner.
-            if (!mpImpl->mPreconditionerName.empty()) {
-                if (mpImpl->mPreconditionerName == "diagonal") {
-                    mpImpl->mpPreconditioner = std::make_shared<cie::linalg::DiagonalOperator<typename Impl::Space>>(
-                        cie::linalg::makeDiagonalOperator<
-                            typename Impl::Value,
-                            typename Impl::Index,
-                            typename Impl::Value>(
-                                lhs, mpImpl->mpSpace));
-                } else {
-                    KRATOS_ERROR
-                    << "unknown preconditioner \""
-                    << mpImpl->mPreconditionerName
-                    << "\"";
-                }
+            if (mpImpl->mPreconditionerName == "diagonal") {
+                mpImpl->mpPreconditioner = std::make_shared<cie::linalg::DiagonalOperator<typename Impl::Space>>(
+                    cie::linalg::makeDiagonalOperator<
+                        typename Impl::Value,
+                        typename Impl::Index,
+                        typename Impl::Value>(
+                            lhs, mpImpl->mpSpace));
+            } else if (mpImpl->mPreconditionerName != "none") {
+                KRATOS_ERROR
+                << "unknown preconditioner \""
+                << mpImpl->mPreconditionerName
+                << "\"";
             }
 
             // Construct the solver.
@@ -184,10 +219,19 @@ void SYCLCGSolver<TS,TD>::InitializeSolutionStep(
 
 template <class TS, class TD>
 bool SYCLCGSolver<TS,TD>::PerformSolutionStep(
-    typename TS::MatrixType&,
+    typename TS::MatrixType& rLhs,
     typename TS::VectorType& rSolution,
     typename TS::VectorType& rRhs) {
         KRATOS_TRY
+            if (!mpImpl->mOrdering.empty()) {
+                cie::reorder<typename Impl::Index,typename Impl::Value>(
+                    mpImpl->mOrdering,
+                    {&rRhs[0], rRhs.size()});
+                cie::reorder<typename Impl::Index,typename Impl::Value>(
+                    mpImpl->mOrdering,
+                    {&rSolution[0], rSolution.size()});
+            }
+
             // Transfer the initial solution and rhs to the device.
             mpImpl->mpSpace->assign(
                 mpImpl->mSolution,
@@ -203,6 +247,20 @@ bool SYCLCGSolver<TS,TD>::PerformSolutionStep(
             mpImpl->mpSpace->assign(
                 {&rSolution[0], rSolution.size()},
                 mpImpl->mSolution);
+
+            if (!mpImpl->mOrdering.empty()) {
+                cie::reverseReorder<typename Impl::Index,typename Impl::Value>(
+                    mpImpl->mOrdering,
+                    {&rLhs.index1_data()[0], rLhs.index1_data().size()},
+                    {&rLhs.index2_data()[0], rLhs.index2_data().size()},
+                    {&rLhs.value_data()[0], rLhs.value_data().size()});
+                cie::reverseReorder<typename Impl::Index,typename Impl::Value>(
+                    mpImpl->mOrdering,
+                    {&rRhs[0], rRhs.size()});
+                cie::reverseReorder<typename Impl::Index,typename Impl::Value>(
+                    mpImpl->mOrdering,
+                    {&rSolution[0], rSolution.size()});
+            }
 
             // Check for convergence.
             const typename Impl::Solver::Statistics stats = mpImpl->mpSolver->getStats().value();
@@ -226,6 +284,7 @@ void SYCLCGSolver<TS,TD>::FinalizeSolutionStep(
             pImpl->mAbsoluteTolerance = mpImpl->mAbsoluteTolerance;
             pImpl->mRelativeTolerance = mpImpl->mRelativeTolerance;
             pImpl->mVerbosity = mpImpl->mVerbosity;
+            pImpl->mReordering = mpImpl->mReordering;
             mpImpl = std::move(pImpl);
         KRATOS_CATCH("")
 }
@@ -239,7 +298,8 @@ Parameters SYCLCGSolver<TS,TD>::GetDefaultParameters() const {
     "max_iterations" : 1e3,
     "absolute_tolerance" : 1e-6,
     "relative_tolerance" : 1e-6,
-    "preconditioner" : "diagonal",
+    "preconditioner" : "none",
+    "reordering" : "none",
     "verbosity" : 1
 })");
 }
